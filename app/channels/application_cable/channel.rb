049e8efccb568cb5c8cee3cc61212ac7fb7c7b41
Jadsia <megankyoung@live.com>
2020-01-05 11:08:03 -0800

    added pusher keys to channel

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,19 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
   end
+
+  require 'pusher'
+
+pusher = Pusher::Client.new(
+  app_id: 'APP_ID',
+  key: 'APP_KEY',
+  secret: 'APP_SECRET',
+  cluster: 'APP_CLUSTER'
+)
+
+  class HelloWorldController < ApplicationController
+    def hello_world
+      pusher.trigger('my-channel', 'my-event', {:message => 'hello world'})
+    end
+  end
 end
